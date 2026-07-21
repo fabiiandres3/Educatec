c2c153a0057369e1627e2ad007c8ba1c19baed65
Fabian <fabiiandres3@gmail.com>
2026-07-21 10:27:08 -0500

    Corrigiendo error de renombres y ruta

--- apps/paneles/urls.py
+++ apps/paneles/urls.py
@@ -5,18 +5,18 @@ from . import views
 urlpatterns = [
     ###################  DASHBOARD DOCENTE  ######################
     path('dashboard_docente/', views.dashboard_docente, name='dashboard_docente'),
-    #path('profesor/cursos/',         views.cursos_profesor,         name='cursos_profesor'),
-    #path('profesor/calificaciones/', views.calificaciones_profesor, name='calificaciones_profesor'),
-    #path('profesor/asistencia/',     views.asistencia_profesor,     name='asistencia_profesor'),
-    #path('profesor/asistencia/historial/', views.historial_asistencia, name='historial_asistencia'),
-    #path('profesor/asistencia/guardar/',views.guardar_asistencia,name='guardar_asistencia'),
-    #path('profesor/configuracion/',  views.configuracion_profesor,  name='configuracion_profesor'),
+    path('docente_cursos/',         views.cursos_docente,         name='cursos_docente'),
+    path('docente_calificaciones/', views.calificaciones_docente, name='calificacion_docente'),
+    path('docente_asistencia/',     views.asistencia_docente,     name='asistencia_docente'),
+    path('docente_asistencia/historial/', views.historial_asistencia, name='historial_asistencia'),
+    path('docente_asistencia/guardar/',views.guardar_asistencia,name='guardar_asistencia'),
+    path('docente_configuracion/',  views.configuracion_docente,  name='configuracion_docente'),
 
     # Estudiante
-    #path('estudiante/',                views.dashboard_estudiante,    name='dashboard_estudiante'),
-    #path('estudiante/materias/',       views.materias_estudiante,     name='materias_estudiante'),
-    #path('estudiante/calificaciones/', views.calificaciones_estudiante, name='calificaciones_estudiante'),
-    #path('estudiante/asistencia/',     views.asistencia_estudiante,   name='asistencia_estudiante'),
-    #path('estudiante/logros/',         views.logros_estudiante,       name='logros_estudiante'),
-    #path('estudiante/configuracion/',  views.configuracion_estudiante, name='configuracion_estudiante'),
+    path('dashboard_docente/',                views.dashboard_estudiante,    name='dashboard_alumno'),
+    path('dashboard_docente/materias/',       views.materias_estudiante,     name='materias_alumno'),
+    path('dashboard_docente/calificaciones/', views.calificaciones_estudiante, name='calificaciones_alumno'),
+    path('dashboard_docente/asistencia/',     views.asistencia_estudiante,   name='asistencia_alumno'),
+    path('dashboard_docente/logros/',         views.logros_estudiante,       name='logros_alumno'),
+    path('dashboard_docente/configuracion/',  views.configuracion_estudiante, name='configuracion_alumno'),
 ]
